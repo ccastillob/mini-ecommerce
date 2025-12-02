7aef2d6cee33e4b6a20d3c5d550fc3d1003082ea
Christofer Castillo <ccastillo993a@gmail.com>
2025-12-02 13:03:07 -0500

    feat: implement components in HomePage with styles

--- src/presentation/screens/home/index.tsx
+++ src/presentation/screens/home/index.tsx
@@ -1,8 +1,15 @@
+import { productsData } from "../../../data/products";
+import { ProductList } from "../../components/products/ProductList";
+
 export const HomePage = () => {
   return (
-    <div>
-      <h1 className="title-color">Inicio - Página Principal</h1>
-      <p className="content-color">Aquí irán los productos...</p>
+    <div className="home-page">
+      <header className="home-page__header">
+        <h1 className="title-color">Productos Disponibles</h1>
+        <p className="content-color">Explora nuestro catálogo y añade productos a tu carrito</p>
+      </header>
+
+      <ProductList products={productsData} />
     </div>
   )
 }

de2eeebe9717ccc34dd24e903f00c02fe7886bcf
Christofer Castillo <ccastillo993a@gmail.com>
2025-12-02 13:05:04 -0500

    feat: implement components in NotFoundPage with styles

--- src/presentation/screens/not-found/index.tsx
+++ src/presentation/screens/not-found/index.tsx
@@ -1,6 +1,23 @@
+import { useNavigate } from "react-router-dom";
 
 export const NotFoundPage = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>NotFoundPage</div>
-  )
+    <div className="not-found-page">
+      <div className="not-found-page__content">
+        <h1 className="not-found-page__title">404</h1>
+        <h2 className="title-color">Página No Encontrada</h2>
+        <p className="content-color">
+          Lo sentimos, la página que buscas no existe.
+        </p>
+        <button
+          className="not-found-page__button"
+          onClick={() => navigate("/")}
+        >
+          Volver al Inicio
+        </button>
+      </div>
+    </div>
+  );
 }
